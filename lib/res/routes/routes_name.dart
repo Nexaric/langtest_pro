1b51cd1deb45bbcf6b186624a1f849d50310e156
Nirinsha <nexaricproject@gmail.com>
2025-06-11 22:51:00 -0700

    routes_name and routes update

--- lib/res/routes/routes_name.dart
+++ lib/res/routes/routes_name.dart
@@ -1,7 +1,152 @@
 class RoutesName {
+  // Root routes from original RoutesName
   static const String splashScreen = "/splash_screen";
   static const String loginScreen = "/login_screen";
   static const String homeScreen = "/home_screen";
   static const String userDetailsScreen = "/user_details_screen";
 
+  // IELTS screens
+  static const String ieltsScreen = "/ielts_screen";
+  static const String ieltsListeningScreen = "/ielts_listening_screen";
+  static const String ieltsReadingScreen = "/ielts_reading_screen";
+  static const String ieltsSpeakingScreen = "/ielts_speaking_screen";
+  static const String ieltsWritingScreen = "/ielts_writing_screen";
+
+  // IELTS Listening sub-screens
+  static const String listeningFeedbackScreen = "/listening_feedback_screen";
+  static const String listeningStrategiesTipsScreen =
+      "/listening_strategies_tips_screen";
+  static const String audioLessonsScreen = "/audio_lessons_screen";
+  static const String audioResultScreen = "/audio_result_screen";
+  static const String audioScreen = "/audio_screen";
+  static const String practiceTestAudioScreen = "/practice_test_audio_screen";
+  static const String practiceTestQuestionsScreen =
+      "/practice_test_questions_screen";
+  static const String practiceTestResultScreen = "/practice_test_result_screen";
+  static const String practiceTestScreen = "/practice_test_screen";
+  static const String practiceTestTimerScreen = "/practice_test_timer_screen";
+
+  // IELTS Reading sub-screens
+  static const String readingFeedbackScreen = "/reading_feedback_screen";
+  static const String readingStrategiesTipsScreen =
+      "/reading_strategies_tips_screen";
+  static const String academicLessonsScreen = "/academic_lessons_screen";
+  static const String academicResultScreen = "/academic_result_screen";
+  static const String lessonScreen = "/lesson_screen";
+  static const String generalLessonsScreen = "/general_lessons_screen";
+  static const String generalResultScreen = "/general_result_screen";
+  static const String generalScreen = "/general_screen";
+
+  // IELTS Speaking sub-screens
+  static const String speakingFeedbackScreen = "/speaking_feedback_screen";
+  static const String speakingStrategiesTipsScreen =
+      "/speaking_strategies_tips_screen";
+
+  // IELTS Writing sub-screens
+  static const String writingFeedbackScreen = "/writing_feedback_screen";
+  static const String writingStrategiesTipsScreen =
+      "/writing_strategies_tips_screen";
+  static const String writingResultScreen = "/writing_result_screen";
+  static const String lessonListScreen = "/lesson_list_screen";
+  static const String lessonResultScreen = "/lesson_result_screen";
+  static const String letterListScreen = "/letter_list_screen";
+  static const String formalLetterScreen = "/formal_letter_screen";
+  static const String informalLetterScreen = "/informal_letter_screen";
+  static const String formalLetterLesson1Screen =
+      "/formal_letter_lesson_1_screen";
+  static const String formalLetterLesson2Screen =
+      "/formal_letter_lesson_2_screen";
+  static const String formalLetterLesson3Screen =
+      "/formal_letter_lesson_3_screen";
+  static const String formalLetterLesson4Screen =
+      "/formal_letter_lesson_4_screen";
+  static const String formalLetterLesson5Screen =
+      "/formal_letter_lesson_5_screen";
+  static const String formalLetterLesson6Screen =
+      "/formal_letter_lesson_6_screen";
+  static const String formalLetterLesson7Screen =
+      "/formal_letter_lesson_7_screen";
+  static const String informalLetterLesson1Screen =
+      "/informal_letter_lesson_1_screen";
+  static const String informalLetterLesson2Screen =
+      "/informal_letter_lesson_2_screen";
+  static const String informalLetterLesson3Screen =
+      "/informal_letter_lesson_3_screen";
+  static const String informalLetterLesson4Screen =
+      "/informal_letter_lesson_4_screen";
+  static const String informalLetterLesson5Screen =
+      "/informal_letter_lesson_5_screen";
+  static const String informalLetterLesson6Screen =
+      "/informal_letter_lesson_6_screen";
+  static const String informalLetterLesson7Screen =
+      "/informal_letter_lesson_7_screen";
+  static const String writingLesson1Screen = "/writing_lesson_1_screen";
+  static const String writingLesson2Screen = "/writing_lesson_2_screen";
+  static const String writingLesson3Screen = "/writing_lesson_3_screen";
+  static const String writingLesson4Screen = "/writing_lesson_4_screen";
+  static const String writingLesson5Screen = "/writing_lesson_5_screen";
+  static const String writingLesson6Screen = "/writing_lesson_6_screen";
+  static const String writingLesson7Screen = "/writing_lesson_7_screen";
+  static const String writingLesson8Screen = "/writing_lesson_8_screen";
+  static const String writingLesson9Screen = "/writing_lesson_9_screen";
+  static const String writingLesson10Screen = "/writing_lesson_10_screen";
+  static const String writingLesson11Screen = "/writing_lesson_11_screen";
+  static const String writingLesson12Screen = "/writing_lesson_12_screen";
+  static const String writingLesson13Screen = "/writing_lesson_13_screen";
+  static const String writingLesson14Screen = "/writing_lesson_14_screen";
+  static const String writingLesson15Screen = "/writing_lesson_15_screen";
+  static const String writingLesson16Screen = "/writing_lesson_16_screen";
+  static const String writingLesson17Screen = "/writing_lesson_17_screen";
+  static const String writingLesson18Screen = "/writing_lesson_18_screen";
+  static const String writingLesson19Screen = "/writing_lesson_19_screen";
+  static const String writingLesson20Screen = "/writing_lesson_20_screen";
+  static const String writingLesson21Screen = "/writing_lesson_21_screen";
+  static const String writingLesson22Screen = "/writing_lesson_22_screen";
+  static const String writingLesson23Screen = "/writing_lesson_23_screen";
+  static const String writingLesson24Screen = "/writing_lesson_24_screen";
+  static const String writingLesson25Screen = "/writing_lesson_25_screen";
+  static const String writingLesson26Screen = "/writing_lesson_26_screen";
+  static const String writingLesson27Screen = "/writing_lesson_27_screen";
+  static const String writingLesson28Screen = "/writing_lesson_28_screen";
+  static const String writingLesson29Screen = "/writing_lesson_29_screen";
+  static const String writingLesson30Screen = "/writing_lesson_30_screen";
+  static const String writingLesson31Screen = "/writing_lesson_31_screen";
+  static const String writingLesson32Screen = "/writing_lesson_32_screen";
+  static const String writingLesson33Screen = "/writing_lesson_32_screen";
+  static const String writingLesson34Screen = "/writing_lesson_34_screen";
+  static const String writingLesson35Screen = "/writing_lesson_35_screen";
+  static const String writingLesson36Screen = "/writing_lesson_36_screen";
+  static const String writingLesson37Screen = "/writing_lesson_37_screen";
+  static const String writingLesson38Screen = "/writing_lesson_38_screen";
+  static const String writingLesson39Screen = "/writing_lesson_39_screen";
+  static const String writingLesson40Screen = "/writing_lesson_40_screen";
+
+  // Home screens
+  static const String dashboardScreen = "/dashboard_screen";
+  static const String notificationScreen = "/notification_screen";
+  static const String aboutUsScreen = "/about_us_screen";
+  static const String menuScreen = "/menu_screen";
+  static const String rateOurAppScreen = "/rate_our_app_screen";
+  static const String shareAppScreen = "/share_app_screen";
+
+  // Payment screens
+  static const String loadingScreen = "/loading_screen";
+  static const String paymentScreen = "/payment_screen";
+  static const String paymentSuccessfulScreen = "/payment_successful_screen";
+
+  // Profile screens
+  static const String editProfileScreen = "/edit_profile_screen";
+  static const String myCoursesScreen = "/my_courses_screen";
+  static const String notificationSettingsScreen =
+      "/notification_settings_screen";
+  static const String preferencesScreen = "/preferences_screen";
+  static const String profileScreen = "/profile_screen";
+  static const String helpSupportScreen = "/help_support_screen";
+  static const String reportProblemScreen = "/report_problem_screen";
+  static const String termsPoliciesScreen = "/terms_policies_screen";
+
+  // Subscription screens
+  static const String oneTimeOfferScreen = "/one_time_offer_screen";
+  static const String subscriptionScreen = "/subscription_screen";
+  static const String subscriptionStatusScreen = "/subscription_status_screen";
 }
